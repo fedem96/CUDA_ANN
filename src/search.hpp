8bb00f0937023c7e19cf93add9d4da3617813f61
fedem96 <federicomagno96@gmail.com>
2019-11-21 10:43:37 +0100

    speeded up sequential version

--- src/search.hpp
+++ src/search.hpp
@@ -1,5 +1,5 @@
-#ifndef CUDA_SEARCH_HPP
-#define CUDA_SEARCH_HPP
+#ifndef SEARCH_HPP
+#define SEARCH_HPP
 
 #include <iostream>
 #include <vector>
@@ -30,15 +30,23 @@ private:
     int datasetSize;
     int spaceDim;
     bool datasetAllocated;
+
+    std::vector<int> nnAllIndexes;
+    std::vector<T> nnAllDistancesSqr;
+
 };
 
 // TODO modificare costruttore: permettere di scegliere se si vuole la versione sequenziale oppure OpenMP (nel secondo caso, permettere di scegliere il numero di thread)
 // io metterei semplicemente un booleano tra i parametri che a seconda t/f esegua le direttive OpenMP o no e l'eventuale numero di thread passato per parametro e messo a default 1
 template<typename T>
-CpuSearch<T>::CpuSearch(T *dataset, int datasetSize, int spaceDim) : dataset(dataset), datasetSize(datasetSize), spaceDim(spaceDim), datasetAllocated(false) {}
+CpuSearch<T>::CpuSearch(T *dataset, int datasetSize, int spaceDim) :
+        dataset(dataset), datasetSize(datasetSize), spaceDim(spaceDim), datasetAllocated(false), nnAllIndexes(datasetSize), nnAllDistancesSqr(datasetSize) {
+    assert(datasetSize > 0);
+}
 
 template<typename T>
-CpuSearch<T>::CpuSearch(const std::vector< std::vector<T> > &dataset_vv) : datasetSize(dataset_vv.size())
+CpuSearch<T>::CpuSearch(const std::vector< std::vector<T> > &dataset_vv) :
+        datasetSize(dataset_vv.size()), nnAllIndexes(dataset_vv.size()), nnAllDistancesSqr(dataset_vv.size())
 {
     assert(datasetSize > 0);
     spaceDim = dataset_vv[0].size();
@@ -57,34 +65,33 @@ T compare (const void * a, const void * b)
 
 template<typename T>
 void CpuSearch<T>::search(T* query, std::vector<int> &nnIndexes, std::vector<T> &nnDistancesSqr, const int &numResults){
-    // TODO implementare la ricerca, versione cpu sequenziale / OpenMP
+    // TODO finire di implementare la ricerca, versione cpu sequenziale / OpenMP
 
-    std::vector<int> nnAllIndexes(datasetSize); // resize dei vetteri per ordinamento ALL
-    std::vector<T> nnAllDistancesSqr(datasetSize);
-
+    T dist, diff;
+    T* k = dataset;
     for(int i=0; i < datasetSize ; i++) {
-        T dist = 0;
+        dist = 0;
         for (int j = 0; j < spaceDim; j++) {
-            const T diff = query[j] - dataset[i * spaceDim + j];
+            // TODO provare loop unrolling
+            k++;
+            diff = query[j] - *k;
             dist = dist + (diff * diff);
         }
 
-        nnAllDistancesSqr[i] = dist; // sarebbe sotto radice ma è uguale
+        // dist is the square of the distance: for efficiency I use it instead of distance to find neighbors
+        nnAllDistancesSqr[i] = dist;
         nnAllIndexes[i] = i;
     }
 
-    // ordinamento di tutto costoso come fare meglio?
-    trust::sort_by_key(&nnAllDistancesSqr[0], &nnAllDistancesSqr[0]+this->datasetSize, &nnAllIndexes[0]);
+    // sort by increasing distance
+    thrust::sort_by_key(&nnAllDistancesSqr[0], &nnAllDistancesSqr[0]+this->datasetSize, &nnAllIndexes[0]);
     // TODO usare std::sort o qsort che sono più veloci. Però c'è da fare in modo che venga ordinato anche il vettore degli indici
     //std::sort(&nnAllDistancesSqr[0], &nnAllDistancesSqr[0]+this->datasetSize);
     //qsort (&nnAllDistancesSqr[0], this->datasetSize, sizeof(T), compare);
 
-    // copia dei primi 100 elementi
+    // copy distances and indexes of nearest neighbors
     std::memcpy(&nnDistancesSqr[0], &nnAllDistancesSqr[0], sizeof(T)  * numResults);
     std::memcpy(&nnIndexes[0], &nnAllIndexes[0], sizeof(int) * numResults);
-
-
-
 }
 
 template<typename T>
@@ -100,4 +107,4 @@ CpuSearch<T>::~CpuSearch() {
 
 
 
-#endif //CUDA_SEARCH_HPP
+#endif //SEARCH_HPP
