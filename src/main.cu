461bac86e350a145223e93f3a2e54d69c6c8ede6
fedem96 <federicomagno96@gmail.com>
2019-11-26 23:55:41 +0100

    changed to C++14; reading command line arguments (to be finished)

--- src/main.cu
+++ src/main.cu
@@ -14,7 +14,7 @@
 #include <vector>
 #include <chrono>
 #include <omp.h>
-#include "csvfile.h"
+#include <unistd.h>
 
 #ifdef __CUDACC__
 #include <cuda.h>
@@ -23,10 +23,10 @@
 #include "utils.cpp"
 #include "read_sift_dataset.cpp"
 #include "search.hpp"
+#include "csvfile.h"
 #ifdef __CUDACC__
 #include "search.cuh"
 #endif
-
 
 
 /** @brief Evaluate a search algorithm.
@@ -63,46 +63,70 @@ int main(int argc, char **argv) {
     #endif
     // TODO prendere i parametri per gli esperimenti da riga di comando
 
-    /* files path definition */
-    // 10^6 examples dataset:
-    //std::string baseFileName = "../data/sift/sift_base.fvecs";
-    //std::string groundtruthFileName = "../data/sift/sift_groundtruth.ivecs";
-    //std::string queryFileName = "../data/sift/sift_query.fvecs";
-    // 10^4 examples dataset:
-    std::string baseFileName = "../data/siftsmall/siftsmall_base.fvecs";
-    std::string groundtruthFileName = "../data/siftsmall/siftsmall_groundtruth.ivecs";
-    std::string queryFileName = "../data/siftsmall/siftsmall_query.fvecs";
+    std::string dataFolder = "../data";
+    char opt;
+    while ((opt = getopt(argc, argv, "d:")) != -1) {
+        switch (opt) {
+            case 'd':
+                dataFolder = optarg;
+                break;
+            default: /* '?' */
+                fprintf(stderr, "Usage: %s [-d path_to_data_folder]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
 
-    /* evaluation parameters */
+    std::cout << "reading dataset in: " << dataFolder << std::endl;
+
+    //// files path definition
+    // 10^6 examples dataset:
+    std::string baseFileName = "/sift/sift_base.fvecs";
+    std::string groundtruthFileName = "/sift/sift_groundtruth.ivecs";
+    std::string queryFileName = "/sift/sift_query.fvecs";
+    // 10^4 examples dataset:
+//    std::string baseFileName = "../data/siftsmall/siftsmall_base.fvecs";
+//    std::string groundtruthFileName = "../data/siftsmall/siftsmall_groundtruth.ivecs";
+//    std::string queryFileName = "../data/siftsmall/siftsmall_query.fvecs";
+
+    //// evaluation parameters
     int numResults = 100;
 
-    /* data structures for dataset */
+    //// data structures for dataset
     std::vector<std::vector<float> > host_dataset_vv;      // datasetSize x spaceDim       <- dataset where to find nearest neighbors
     std::vector<std::vector<float> > host_queries_vv;      // numQueries  x spaceDim       <- test samples
     std::vector<std::vector<int> > host_grTruth_vv;        // numQueries  x 100            <- first 100 nearest neighbors for each test sample
 
-    /* reading of dataset, queries and groundtruth */
+    //// reading of dataset, queries and groundtruth
     std::cout << "Reading dataset" << std::endl;
-    assert((readVecsFile<float, float>(baseFileName, host_dataset_vv, false)));
-    std::cout << "\nReading queries" << std::endl;
-    assert((readVecsFile<float, float>(queryFileName, host_queries_vv, false)));
-    std::cout << "\nReading groundtruth for queries" << std::endl;
-    assert((readVecsFile<int, int>(groundtruthFileName, host_grTruth_vv, false)));
+    // TODO safely join paths dataFolder and ... (provare con libreria boost)
+    assert((readVecsFile<float, float>(dataFolder + baseFileName, host_dataset_vv, true)));
+    std::cout << "Reading queries" << std::endl;
+    assert((readVecsFile<float, float>(dataFolder + queryFileName, host_queries_vv, true)));
+    std::cout << "Reading groundtruth for queries" << std::endl;
+    assert((readVecsFile<int, int>(dataFolder + groundtruthFileName, host_grTruth_vv, true)));
 
     // dataset slice (to do quick tests) TODO remove in final version
 //    const int numExamples = 30000;
 //    host_dataset_vv = std::vector< std::vector<float> >(host_dataset_vv.begin(), host_dataset_vv.begin() + numExamples);
 //    host_dataset_vv.resize(numExamples);
 
-    /* constants initialization */
-    const int datasetSize = host_dataset_vv.size();
-    assert(datasetSize > 0);
-    const int spaceDim = 128; //FIXME problem whit release mode if i use  spaceDim = host_dataset_vv[0].size(); why?
-    const int numQueries = host_queries_vv.size();
-    assert(host_queries_vv.size() == host_grTruth_vv.size());          // host_queries_vv and host_grTruth_vv must have same length
-    assert(numResults <= host_grTruth_vv[0].size());            // assert(numResults <= 100)
+    //// constants initialization
+    const int datasetSize = static_cast<const int>(host_dataset_vv.size());
+    std::cout << "Dataset size: " << datasetSize << std::endl;
 
-    /* some print to understand data */
+    std::cout << "Space size: " << host_dataset_vv.at(0).size() << std::endl;
+    const int spaceDim = 128; //FIXME problem whit release mode if I use  spaceDim = host_dataset_vv[0].size(); why?
+
+    const int numQueries = static_cast<const int>(host_queries_vv.size());
+    std::cout << "Number of queries: " << host_dataset_vv.size() << std::endl;
+
+    assert(datasetSize > 0);
+    assert(spaceDim > 0);
+    assert(numQueries > 0);
+    assert(host_queries_vv.size() == host_grTruth_vv.size());          // host_queries_vv and host_grTruth_vv must have same length
+    assert(numResults <= host_grTruth_vv.at(0).size());            // assert(numResults <= 100)
+
+    //// some print to understand data
     //dataPrint(host_dataset_vv, host_grTruth_vv, host_queries_vv);
 //    for(int i=0; i<host_grTruth_vv.size(); i++)
 //        for(int j=0; j<host_grTruth_vv[0].size(); j++)
@@ -110,7 +134,7 @@ int main(int argc, char **argv) {
 //            	cout << "ERRORE: "<< i << " " << j<< " "<< host_grTruth_vv[i][j] << std::endl;
 
 
-    /* data conversion */
+    //// data conversion
     // convert queries from vector of vectors into raw pointer
     // TODO capire perché con la pinned va più lento mentre invece dovrebbe essere più veloce (forse perché il dato trasferito è piccolo, ogni query è 512 byte)
     float *host_queries_ptr;
@@ -123,7 +147,7 @@ int main(int argc, char **argv) {
         std::memcpy(host_queries_ptr + (i * spaceDim), &host_queries_vv[i][0], sizeof(float) * spaceDim);
     }
 
-    /* evaluation: for each implementation, execute search and measure elapsed time */
+    //// evaluation: for each implementation, execute search and measure elapsed time
     Search<float> *s;
     auto start = std::chrono::high_resolution_clock::now();
     //// CPU evaluation
@@ -142,11 +166,11 @@ int main(int argc, char **argv) {
 
         for(int numCores = 1; numCores <= maxThreads; numCores++){  // openmp directive for the number of cores
             //TODO add the command for create a csv here like " alg_version;num_threads;dataset_size;time;name "
+            std::cout << "Test on CPU, cores: " << numCores << std::endl;
             start = std::chrono::high_resolution_clock::now();
             s = new CpuSearch<float>(host_dataset_vv, numCores);
             std::chrono::duration<double> cpuInitTime = std::chrono::high_resolution_clock::now() - start;
-            std::chrono::duration<double> cpuEvalTime = evaluate<float>(s, host_queries_ptr, host_grTruth_vv, numQueries,
-                                                                        numResults, true);
+            std::chrono::duration<double> cpuEvalTime = evaluate<float>(s, host_queries_ptr, host_grTruth_vv, numQueries, numResults, true);
             std::cout << "CPU (Cores:" << numCores << ") init time: " << cpuInitTime.count() << std::endl;
             std::cout << "CPU (Cores:" << numCores << ") eval time: " << cpuEvalTime.count() << std::endl;
             csv << numCores << datasetSize << cpuInitTime.count() << cpuEvalTime.count() << cpuInitTime.count() + cpuEvalTime.count() << "processore" << endrow;
@@ -177,7 +201,7 @@ int main(int argc, char **argv) {
 template <typename T>
 std::chrono::duration<double> evaluate(Search<T> *s, T* queries_ptr, std::vector <std::vector<int>> &groundTruth, const int &numQueries, int &numResults, bool mustCheckCorrectness){
 
-    /* data structures for query */
+    //// data structures for query
     std::vector<std::vector<int> > nnAllIndexes(numQueries, std::vector<int>(numResults));    // numQueries x numResults
     std::vector<std::vector<T> > nnAllDistancesSqr(numQueries, std::vector<float>(numResults));     // numQueries x numResults
 
@@ -193,9 +217,11 @@ std::chrono::duration<double> evaluate(Search<T> *s, T* queries_ptr, std::vector
     std::chrono::duration<double> elapsedTime = end-start;
 
     // eventually check for correctness
-    if(mustCheckCorrectness)
+    if(mustCheckCorrectness) {
         //assert(checkCorrectness(groundTruth, nnAllIndexes, nnAllDistancesSqr));
-        checkCorrectness(groundTruth, nnAllIndexes, nnAllDistancesSqr);
+        bool c = checkCorrectness(groundTruth, nnAllIndexes, nnAllDistancesSqr);
+        std::cout << "correctness: " << (c?"true":"false") << std::endl;
+    }
 
     return elapsedTime;
 }
