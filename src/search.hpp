c2a113c6252fcbcd724d35770304f931f5fdd96e
fedem96 <federicomagno96@gmail.com>
2019-12-01 22:23:19 +0100

    better performances for OpenMP version: multithread sort, merge

--- src/search.hpp
+++ src/search.hpp
@@ -20,6 +20,9 @@ struct Neighbor {
     bool operator==(const Neighbor& rhs) { return distance == rhs.distance; }
 };
 
+template<typename InputIterator, typename OutputIterator>
+OutputIterator merge(InputIterator, InputIterator, InputIterator, InputIterator, OutputIterator);
+
 template <typename T>
 class Search{
 public:
@@ -32,7 +35,7 @@ template <typename T>
 class CpuSearch : public Search<T> {
 public:
     CpuSearch(T* dataset, int rows, int cols, int numCores=0);
-    CpuSearch(const std::vector< std::vector<T> > &dataset, int numCores=0);
+    CpuSearch(const std::vector< std::vector<T> > &dataset, int numThreads=0);
     void search(T* query, std::vector<int> &nnIndexes, std::vector<T> &nnDistancesSqr, const int &numResults) override;
     int getSpaceDim() override;
     virtual ~CpuSearch();
@@ -41,10 +44,8 @@ private:
     int datasetSize;
     int spaceDim;
     bool datasetAllocated;
-    int numCores;
+    int numThreads;
 
-    std::vector<int> nnAllIndexes;
-    std::vector<T> nnAllDistancesSqr;
     std::vector<Neighbor<T> > nearestNeighbors;
 
     T* dists;
@@ -53,13 +54,13 @@ private:
 
 // TODO the contructor must specify the number of cores for the openMP version
 template<typename T>
-CpuSearch<T>::CpuSearch(T *dataset, int datasetSize, int spaceDim, int numCores) : dataset(dataset), datasetSize(datasetSize), spaceDim(spaceDim), datasetAllocated(false), numCores(numCores), nnAllIndexes(datasetSize), nnAllDistancesSqr(datasetSize), nearestNeighbors(datasetSize) {
+CpuSearch<T>::CpuSearch(T *dataset, int datasetSize, int spaceDim, int numCores) : dataset(dataset), datasetSize(datasetSize), spaceDim(spaceDim), datasetAllocated(false), numThreads(numCores), nearestNeighbors(datasetSize) {
     assert(datasetSize > 0);
     dists = new T[spaceDim];
 }
 
 template<typename T>
-CpuSearch<T>::CpuSearch(const std::vector< std::vector<T> > &dataset_vv, int numCores) : datasetSize(dataset_vv.size()), numCores(numCores), nnAllIndexes(dataset_vv.size()), nnAllDistancesSqr(dataset_vv.size()), nearestNeighbors(dataset_vv.size())
+CpuSearch<T>::CpuSearch(const std::vector< std::vector<T> > &dataset_vv, int numThreads) : datasetSize(dataset_vv.size()), numThreads(numThreads), nearestNeighbors(dataset_vv.size())
 {
     assert(datasetSize > 0);
     spaceDim = dataset_vv.at(0).size();
@@ -79,23 +80,45 @@ T comparator(const void * a, const void * b){
 template<typename T>
 void CpuSearch<T>::search(T* query, std::vector<int> &nnIndexes, std::vector<T> &nnDistancesSqr, const int &numResults){
 
-    omp_set_num_threads(numCores);
-    #pragma omp parallel for
-    for(int i=0; i < datasetSize ; i++) {
-        T dist = 0;
-        T* dataset_tmp = dataset + (i*spaceDim);
-        for (int j = 0; j < spaceDim; j++) {
-            const T diff = query[j] - dataset_tmp[j];
-            dist += diff * diff;
+    omp_set_num_threads(numThreads);
+    int chunkSize = datasetSize / numThreads;
+    int remainder = datasetSize - (chunkSize * numThreads);
+
+    #pragma omp parallel
+    {
+        // calculate distances
+        #pragma omp for
+        for (int i = 0; i < datasetSize; i++) {
+            T dist = 0;
+            T *dataset_tmp = dataset + (i * spaceDim);
+            for (int j = 0; j < spaceDim; j++) {
+                const T diff = query[j] - dataset_tmp[j];
+                dist += diff * diff;
+            }
+
+            // dist is the square of the distance: for efficiency I use it instead of distance to find neighbors
+            nearestNeighbors[i].distance = dist;
+            nearestNeighbors[i].index = i;
         }
 
-        // dist is the square of the distance: for efficiency I use it instead of distance to find neighbors
-        nearestNeighbors[i].distance = dist;
-        nearestNeighbors[i].index = i;
+        // sort by increasing distance
+        #pragma omp for
+        for (int i = 0; i < numThreads; i++) {  // each thread sorts its chunk
+            qsort(&nearestNeighbors[i * chunkSize], chunkSize + (i + 1 == numThreads ? remainder : 0), sizeof(Neighbor<T>), comparator);
+        }
     }
 
-    // sort by increasing distance
-    qsort (&nearestNeighbors[0], this->datasetSize, sizeof(Neighbor<T>), comparator);
+    // in single-thread, no merge is required
+    if(numThreads != 1) {
+        // sorted chunk needs to be merged
+        Neighbor<T> *tmp = new Neighbor<T>[numResults];
+        for (int i = 1; i < numThreads; i++) {
+            std::memcpy(tmp, &nearestNeighbors[0], sizeof(T)*numResults);
+            merge(tmp, tmp + numResults, &nearestNeighbors[i * chunkSize], &nearestNeighbors[i * chunkSize + numResults], &nearestNeighbors[0]);
+        }
+        delete [] tmp;
+        // at the end of merge, nearest neighbors are the first elements of the array nearestNeighbors
+    }
 
     // copy distances and indexes of nearest neighbors
     for(int i = 0; i < numResults; i++) {
@@ -116,6 +139,16 @@ CpuSearch<T>::~CpuSearch() {
     delete [] dists;
 }
 
+
+template<typename InputIterator, typename OutputIterator>
+OutputIterator merge(InputIterator ii1, InputIterator end1, InputIterator ii2, InputIterator end2, OutputIterator oi){
+    while(ii1 < end1 || ii2 < end2)
+        if(ii1 < end1 && *ii1 <= *ii2)
+            *(oi++) = *(ii1++);
+        else
+            *(oi++) = *(ii2++);
+    return oi;
+}
 
 
 #endif //CUDA_SEARCH_HPP
